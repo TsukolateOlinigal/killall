e2cb9dd7244a07a86bf82cf2a12710e62de1a1f0
TsukolateOlinigal <lindalindahelo@gmail.com>
2026-09-08 17:58:10 -0300

    Update stealaegghop.lua

--- stealaegghop.lua
+++ stealaegghop.lua
@@ -1,6 +1,21 @@
 local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 
-local player = Players.LocalPlayer
+local PlaceId = game.PlaceId
+local servers = {}
 
-TeleportService:Teleport(game.PlaceId, player)
+local data = HttpService:JSONDecode(
+    game:HttpGet("https://games.roblox.com/v1/games/"..PlaceId.."/servers/Public?limit=100")
+)
+
+for _, server in pairs(data.data) do
+    if server.id ~= game.JobId and server.playing < server.maxPlayers then
+        table.insert(servers, server.id)
+    end
+end
+
+if #servers > 0 then
+    local randomServer = servers[math.random(1, #servers)]
+    TeleportService:TeleportToPlaceInstance(PlaceId, randomServer, Players.LocalPlayer)
+end
